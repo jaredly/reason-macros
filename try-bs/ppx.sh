9f40b023ebd235127270a714218a8badb0285fbc
Jared Forsyth <jared@jaredforsyth.com>
2019-07-28 22:44:44 -0600

    try to get windows working in azure

--- try-bs/ppx.sh
+++ try-bs/ppx.sh
@@ -1,2 +1,2 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-$DIR/../_esy/default/build/install/default/bin/ppx $DIR/Shared.macros.re $1 $2
+$DIR/../_build/install/default/bin/ppx $DIR/Shared.macros.re $1 $2
